9f40a4318784f80891703cac6eacfef0a4470a80
Christopher Justice <19810192+xodus899@users.noreply.github.com>
2021-12-11 15:29:14 -0500

    Update scripts.js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,9 +1,6 @@
-let date = new Date();
-let year = date.getFullYear();
-let element = document.getElementById("copyright")
-
 const getYear = () => {
+  let date = new Date();
+  let year = date.getFullYear();
+  let element = document.getElementById("copyright");
   return element.innerHTML = `Copyright C.Justice ${year}`;
-}
-
-console.log(getYear())
+};
