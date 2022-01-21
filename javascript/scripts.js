ec21441be21aefb59db0b5ed77b8762f665c3881
chris <19810192+xodus899@users.noreply.github.com>
2022-01-20 21:10:36 -0500

    remove scroll to top

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -2,15 +2,3 @@ const getYear = () => {
   return document.getElementById("copyright").innerHTML = `Copyright C.Justice ${new Date().getFullYear()}`
 }
 getYear();
-
-
-const scroll = () => {
-  document.querySelector(".scroll-to-top").addEventListener('click', () => {
-    window.scrollTo({
-      top:0,
-      behavior:'smooth'
-    });
-  })
-};
-
-scroll();
